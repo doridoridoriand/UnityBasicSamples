30baf7aaa13ee64646f1258a3374745b907aad59
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-09-07 23:54:41 +0900

    fix syntax indent

--- Roll-a-Ball/Assets/Scripts/CameraController.cs
+++ Roll-a-Ball/Assets/Scripts/CameraController.cs
@@ -3,17 +3,17 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	public  GameObject  player; 
-	private Vector3  offset; 
+  public  GameObject  player; 
+  private Vector3  offset; 
 
-	// Use this for initialization
-	void Start () {
-		offset = transform.position - player.transform.position; 
-	}
-	
-	//
-	void LateUpdate () {
-		transform.position = player.transform.position + offset;
-	
-	}
+  // Use this for initialization
+  void Start () {
+    offset = transform.position - player.transform.position; 
+  }
+
+  //
+  void LateUpdate () {
+    transform.position = player.transform.position + offset;
+
+  }
 }
--- Roll-a-Ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -3,37 +3,37 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	
-	public float speed;
-	public Text countText;
-	private Rigidbody  rbody;
-	private int count;
 
-	// Start is  called  at initiarize
-	void Start () {
-		rbody = GetComponent<Rigidbody> ();
-		count = 0;
-		SetCountText();
-	}
+  public float speed;
+  public Text countText;
+  private Rigidbody  rbody;
+  private int count;
 
-	void FixedUpdate() {
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-		
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical); 
-  		 rbody.AddForce (movement * speed); 
- 	}
+  // Start is  called  at initiarize
+  void Start () {
+    rbody = GetComponent<Rigidbody> ();
+    count = 0;
+    SetCountText();
+  }
 
-	 void OnTriggerEnter(Collider other) {
-		if (other.gameObject.CompareTag ("Cubes")) {
-			other.gameObject.SetActive (false);
-			count = count + 1;
-			SetCountText();
-			return;
-		}
-	}
+  void FixedUpdate() {
+    float moveHorizontal = Input.GetAxis ("Horizontal");
+    float moveVertical = Input.GetAxis ("Vertical");
 
- 	 void SetCountText() { 
-		countText.text = "count: " + count.ToString();
-	}
+    Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical); 
+    rbody.AddForce (movement * speed); 
+  }
+
+  void OnTriggerEnter(Collider other) {
+    if (other. gameObject.CompareTag ("Cubes")) {
+      other.gameObject.SetActive (false);
+      count = count + 1;
+      SetCountText();
+      return;
+    }
+  }
+
+  void SetCountText() { 
+    countText.text = "count: " + count.ToString();
+  }
 }
