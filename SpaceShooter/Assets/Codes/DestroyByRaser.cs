3aa6321edf4b362d7d91370e18201db4b51db755
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-09-09 12:03:12 +0900

    add obstaacles which has explosion effects

---
+++ SpaceShooter/Assets/Codes/DestroyByRaser.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByRaser : MonoBehaviour {
+
+  public GameObject explosion;
+
+  void OnTriggerEnter(Collider other) {
+    // gameObjectの名前がBoundaryだったら以降の処理をやめる
+    // (枠からはみ出さないようにしている透明エリアを例外とするため。そうじゃないと実行した瞬間にOnTriggerEnterで自身が消えてしまうから)
+    if (other.name == "Boundary") {
+      return;
+    }
+    Destroy(other.gameObject);
+    Destroy(gameObject);
+    Instantiate(explosion, transform.position, transform.rotation);
+  }
+}
