ba3263800bd3c6ffdc7a1863140a54be12b70ff8
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-09-10 17:29:31 +0900

    change initial rotation

--- ObjectTest/Assets/Codes/RandomObjectGenerator.cs
+++ ObjectTest/Assets/Codes/RandomObjectGenerator.cs
@@ -8,13 +8,14 @@ public class RandomObjectGenerator : MonoBehaviour {
 
   // Use this for initialization
   void Start () {
-    for (int i = 0; i <= 500; i++) {
+    for (int i = 0; i <= 800; i++) {
       Instantiate(targetGameObject, new Vector3(Random.Range(MinRange, MaxRange),
             Random.Range(MinRange, MaxRange),
             Random.Range(MinRange, MaxRange)),
-            Quaternion.Euler(Random.Range(0, 180),
-                             Random.Range(0, 180),
-                             Random.Range(0, 180)));
+            Quaternion.Euler(0.0f, 0.0f, -30.0f));
+//          Quaternion.Euler(Random.Range(0, 180),
+//                             Random.Range(0, 180),
+//                             Random.Range(0, 180)));
     }
   }
 }
